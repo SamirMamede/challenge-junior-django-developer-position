ee1cb58f7b4b9dca002acd3d366c20cdcb1d5f59
SamirMamede <anuarsamir@gmail.com>
2024-09-08 23:36:04 -0400

    model review

--- film_review/reviews/models.py
+++ film_review/reviews/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,19 +19,7 @@ class Review(models.Model):
     status = models.IntegerField(choices=Status.choices, default=Status.DRAFT)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=Rating.choices, default=Rating.BAD)
-    
-
-"""
-model reviews
-
-id: A primary key integer field that uniquely identifies each review.
-title: A string field to store the title of the review limited to 200 characters.
-status: An integer field to store the status of the review (Draft or Published).
-body: A text field to store the main content of the review.
-author_id: An integer field to create a relationship between the review and the user who authored it, referencing the User table's primary key.
-rating: An integer field to store the rating of the review (BAD, POOR, FAIR, GOOD, EXCELLENT, EXCEPTIONAL).
-published_at: A datetime field to store when the review was published.
-created_at: A datetime field to store when the review record was created.
-updated_at: A datetime field to store when the review record was lats updated.
-"""
+    rating = models.IntegerField(choices=Rating.choices, default=Rating.GOOD)
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

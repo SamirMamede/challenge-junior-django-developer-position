5bb667179889c7d17c88e21d1f1427ea6002eabb
SamirMamede <anuarsamir@gmail.com>
2024-09-09 22:39:56 -0400

    first migration, update model review, create superuser and start admin registers

--- film_review/reviews/admin.py
+++ film_review/reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["title", "published_at", "author", "rating"]
+    list_filter = ["author", "status","rating"]
--- film_review/reviews/models.py
+++ film_review/reviews/models.py
@@ -23,3 +23,6 @@ class Review(models.Model):
     published_at = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title

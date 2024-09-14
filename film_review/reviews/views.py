58e585a5f0fa4a1148db1bce9ced17b9b265214c
SamirMamede <anuarsamir@gmail.com>
2024-09-13 20:45:35 -0400

    update views, urls

--- film_review/reviews/views.py
+++ film_review/reviews/views.py
@@ -5,3 +5,6 @@ from reviews.models import Review
 def review_list(request):
     reviews = Review.objects.filter(status=Review.Status.PUBLISHED)
     return render(request, "reviews/list.html", {"reviews": reviews})
+
+def review_datail(request, id):
+    pass

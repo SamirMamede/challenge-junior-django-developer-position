13900696392b693d8fae8984850ce98655f0abb7
SamirMamede <anuarsamir@gmail.com>
2024-09-21 22:25:42 -0400

    Fixed 404 error when Review object is not found by using get_object_or_404

--- film_review/reviews/views.py
+++ film_review/reviews/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
 from reviews.models import Review
 
 def review_list(request):
@@ -7,5 +6,5 @@ def review_list(request):
     return render(request, "reviews/list.html", {"reviews": reviews})
 
 def review_datail(request, id):
-    review = Review.published.get(id=id)
+    review = get_object_or_404(Review.published, id=id)
     return render(request, "reviews/detail.html", {"review": review})
